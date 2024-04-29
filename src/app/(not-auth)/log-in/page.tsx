c518e8d675f65f5a6f9f9ca2cc060d772f795313
ICE0208 <doky3307@naver.com>
2024-04-29 20:43:01 +0900

    feat: 로그인 페이지

---
+++ src/app/(not-auth)/log-in/page.tsx
@@ -0,0 +1,49 @@
+import BlueSubmitButton from "@/components/buttons/BlueSubmitButton";
+import cls from "@/utils/cls";
+import Link from "next/link";
+
+export default function LoginPage() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center bg-[#F0F4F9] px-16 py-8">
+      <h1 className="mb-4 text-[44px] font-bold">대동밥 지도</h1>
+      <form
+        className={cls(
+          "relative mb-20 flex w-full max-w-[700px] flex-col items-center rounded-3xl bg-white px-16 py-8",
+          "shadow-[0_3px_10px_rgb(0,0,0,0.2)]",
+        )}
+      >
+        <h1 className="mb-6 text-3xl font-medium">Login</h1>
+        <div className="flex w-[280px] flex-col gap-1">
+          <div className="flex w-full justify-between">
+            <span className="mr-3 flex-1 text-right">아이디</span>
+            <input
+              className="w-[180px] rounded-lg border-2 px-1"
+              placeholder="abc1234"
+              name="id"
+              type="text"
+              required
+            />
+          </div>
+          <div className="flex w-full justify-between">
+            <span className="mr-3 flex-1 text-right">비밀번호</span>
+            <input
+              className="w-[180px] rounded-lg border-2 px-1"
+              placeholder="1234*#"
+              name="password"
+              type="password"
+              required
+            />
+          </div>
+        </div>
+        <span className="mt-8"></span>
+        <BlueSubmitButton text="로그인" />
+        <Link
+          className="absolute bottom-8 right-10 border-b-[1.5px] border-black"
+          href="/create-account"
+        >
+          가입할래요 &rarr;
+        </Link>
+      </form>
+    </div>
+  );
+}
---
+++ src/components/buttons/BlueSubmitButton.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import cls from "@/utils/cls";
+import { ButtonHTMLAttributes } from "react";
+import { useFormStatus } from "react-dom";
+
+interface ButtonProps {
+  text: string;
+}
+
+export default function BlueSubmitButton({
+  text,
+  onClick,
+}: ButtonProps & ButtonHTMLAttributes<HTMLButtonElement>) {
+  const { pending } = useFormStatus();
+
+  return (
+    <button
+      className={cls(
+        "rounded-xl bg-blue-600 px-6 py-2 text-white",
+        "hover:bg-blue-700 disabled:bg-blue-400 disabled:hover:bg-blue-400",
+      )}
+      onClick={onClick}
+      disabled={pending}
+    >
+      {pending ? (
+        <div className="flex items-center gap-[4px]">
+          <span>로딩중</span>
+          <div className="mx-[4px] animate-spin text-[8px] font-semibold">
+            |
+          </div>
+        </div>
+      ) : (
+        <span>{text}</span>
+      )}
+    </button>
+  );
+}
