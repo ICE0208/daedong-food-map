00c360340bb9537b9cef8970d8aa8604cfd9b67f
ICE0208 <doky3307@naver.com>
2024-05-14 23:57:15 +0900

    feat: 프로필 사진 영역 추가

--- src/app/(auth)/talk/[id]/components/Comment.tsx
+++ src/app/(auth)/talk/[id]/components/Comment.tsx
@@ -16,10 +16,16 @@ export default function Comment({
 }: CommentProps) {
   return (
     <div className="flex flex-col rounded-xl bg-neutral-50 px-6 py-5">
-      <span className="text-[18px] font-semibold leading-6">{author}</span>
-      <span className="text-[12px]">{formattedData}</span>
+      <div className="flex items-center gap-2">
+        <div className="aspect-square w-10 rounded-full bg-blue-300"></div>
+        <div className="flex flex-col">
+          <span className="text-[18px] font-semibold leading-6">{author}</span>
+          <span className="text-[12px]">{formattedData}</span>
+        </div>
+      </div>
+      <div className="my-[4px]" />
       <span className="whitespace-pre-wrap break-words">{content}</span>
-      <div className="my-1" />
+      <div className="my-[2px]" />
       <div className="flex items-center gap-2">
         <div className="flex items-center gap-[1px]">
           <SVGButton svg={HeartSVG} size={5} color="rgb(252, 84, 151)" />
