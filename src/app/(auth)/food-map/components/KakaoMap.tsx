5eac83c8bedf5da1948569e65296794614534148
ICE0208 <doky3307@naver.com>
2024-05-16 00:17:39 +0900

    feat: 주변 음식점을 지도에 표시하기

--- src/app/(auth)/food-map/components/KakaoMap.tsx
+++ src/app/(auth)/food-map/components/KakaoMap.tsx
@@ -1,11 +1,16 @@
+import { SearchKeywordResponse } from "@/types/apiTypes";
+import { useEffect } from "react";
 import { Map, MapMarker, useKakaoLoader } from "react-kakao-maps-sdk";
+
+import IMAGE from "@/assets/images/red-dot.png";
 
 interface KakaoMapProps {
   lat: number;
   lng: number;
+  restaurantsData?: SearchKeywordResponse["documents"];
 }
 
-export default function KakaoMap({ lat, lng }: KakaoMapProps) {
+export default function KakaoMap({ lat, lng, restaurantsData }: KakaoMapProps) {
   useKakaoLoader({
     appkey: process.env.NEXT_PUBLIC_KAKAOJSKEY!,
     libraries: ["clusterer", "services"],
@@ -23,9 +28,18 @@ export default function KakaoMap({ lat, lng }: KakaoMapProps) {
         width: "100%",
         height: "100%",
       }}
-      level={3}
+      level={4}
     >
-      <MapMarker position={{ lat, lng }}>
+      {restaurantsData?.map((data) => (
+        <MapMarker
+          key={data.id}
+          position={{ lat: Number(data.y), lng: Number(data.x) }}
+        ></MapMarker>
+      ))}
+      <MapMarker
+        position={{ lat, lng }}
+        image={{ src: IMAGE.src, size: { width: 24, height: 24 } }}
+      >
         <div style={{ color: "#000" }}>Hello World!</div>
       </MapMarker>
     </Map>
--- src/app/(auth)/food-map/page.tsx
+++ src/app/(auth)/food-map/page.tsx
@@ -3,6 +3,7 @@
 import { useEffect, useState } from "react";
 import { CurrentPositionButton, KakaoMap } from "./components";
 import { getNearRestaurant } from "./actions";
+import { SearchKeywordResponse } from "@/types/apiTypes";
 
 export interface Position {
   lat: number;
@@ -14,12 +15,13 @@ export default function FoodMapPage() {
     lat: 33.450701,
     lng: 126.570667,
   });
+  const [data, setData] = useState<SearchKeywordResponse | null>(null);
 
   useEffect(() => {
     if (!position) return;
     const action = async () => {
       const result = await getNearRestaurant(position);
-      console.log(result);
+      setData(result);
     };
 
     action();
@@ -28,7 +30,11 @@ export default function FoodMapPage() {
   return (
     <div className="flex w-full flex-col items-center px-24 py-8">
       <div className="h-[600px] w-full max-w-[1200px] overflow-hidden rounded-xl bg-neutral-100">
-        <KakaoMap lat={position.lat} lng={position.lng} />
+        <KakaoMap
+          restaurantsData={data?.documents}
+          lat={position.lat}
+          lng={position.lng}
+        />
       </div>
       <CurrentPositionButton setPosition={setPosition} />
     </div>
