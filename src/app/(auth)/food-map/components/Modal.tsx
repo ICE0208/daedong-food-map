8d1ef573a54562fda47d080d6498a88eafab9a73
ICE0208 <doky3307@naver.com>
2024-05-30 02:32:13 +0900

    feat: 상세 버튼, 닫기 버튼 추가

--- src/app/(auth)/food-map/components/Modal.tsx
+++ src/app/(auth)/food-map/components/Modal.tsx
@@ -1,5 +1,11 @@
+"use client";
+
+import SVGButton from "@/components/SVGButton";
+import LinkSVG from "@/icons/LinkSVG";
+import XMarkSVG from "@/icons/XMarkSVG";
 import { SearchKeywordResponse } from "@/types/apiTypes";
 import formatCategoryName from "@/utils/splitCategory";
+import { useRouter } from "next/navigation";
 import { Roadview } from "react-kakao-maps-sdk";
 
 interface ModalProps {
@@ -9,9 +15,9 @@ interface ModalProps {
 }
 
 const Modal = ({ selectedId, onExit, data }: ModalProps) => {
-  if (!data) return <div>Error</div>;
+  const router = useRouter();
 
-  console.log(data);
+  if (!data) return <div>Error</div>;
 
   return (
     <div className="fixed left-0 top-0 z-50 flex h-full w-full items-center justify-center bg-[#00000096] p-12">
@@ -19,7 +25,17 @@ const Modal = ({ selectedId, onExit, data }: ModalProps) => {
         className="absolute left-0 top-0 -z-10 h-full w-full"
         onClick={onExit}
       ></div>
-      <div className="flex w-[1200px] flex-col items-center overflow-hidden rounded-3xl bg-neutral-100 px-8 py-10">
+      <div className="relative flex w-[1200px] flex-col items-center overflow-hidden rounded-3xl bg-neutral-100 px-8 py-10">
+        <div className="absolute right-8 top-10 flex items-center gap-2">
+          <SVGButton
+            svg={LinkSVG}
+            size={7}
+            onClick={() => {
+              router.push(`/restaurant/${data.id}`);
+            }}
+          />
+          <SVGButton svg={XMarkSVG} size={8} onClick={onExit} />
+        </div>
         <h1 className="mb-2 text-3xl font-semibold">{data.place_name}</h1>
         <span className="text-lg">{data.road_address_name}</span>
         <span className="text-lg">{data.phone}</span>
