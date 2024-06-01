4d5a0fe848af917f7c6713d81442a546f19d3423
ICE0208 <doky3307@naver.com>
2024-06-01 21:47:50 +0900

    feat: 식당 기본 정보 표시

--- src/app/(auth)/restaurant/[id]/page.tsx
+++ src/app/(auth)/restaurant/[id]/page.tsx
@@ -1,4 +1,6 @@
 import db from "@/libs/db";
+import formatCategoryName from "@/utils/splitCategory";
+import { redirect } from "next/navigation";
 
 interface RestaurantPageProps {
   params: {
@@ -15,16 +17,32 @@ export default async function RestaurantPage({ params }: RestaurantPageProps) {
     },
   });
 
-  console.log(data);
+  if (!data) {
+    console.error("해당 식당 id가 db에 없음");
+    return redirect("/");
+  }
 
   return (
-    <div className="flex w-full flex-1 px-32 py-12">
+    <main className="flex w-full flex-1 px-32 py-12">
       <div className="relative flex w-full gap-8">
-        <div className="relative w-full overflow-hidden rounded-xl bg-neutral-50">
-          {id}
+        <div className="relative w-full overflow-hidden rounded-xl bg-neutral-50 p-14">
+          <div className="flex gap-8">
+            {/* 관련 이미지 */}
+            <div className="size-[200px] rounded-md bg-gray-200" />
+            {/* 식당 정보 */}
+            <div className="flex flex-col py-4">
+              <h1 className="mb-2 text-[42px] font-semibold ">{data.name}</h1>
+              <span className="flex-1 text-lg leading-[0px]">
+                {formatCategoryName(data.category)}
+              </span>
+              <span className="text-[20px]">{data.address}</span>
+              <span className="text-[20px]">{data.phone}</span>
+            </div>
+          </div>
         </div>
+        {/* ------- */}
         <div className="relative w-[600px] overflow-hidden rounded-xl bg-neutral-50"></div>
       </div>
-    </div>
+    </main>
   );
 }
