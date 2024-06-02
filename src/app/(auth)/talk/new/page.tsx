b4beb62337d4e587296b7512fd9d09178f8207ed
ICE0208 <doky3307@naver.com>
2024-06-02 23:19:01 +0900

    feat: 잡담 등록 구현

--- src/app/(auth)/talk/new/page.tsx
+++ src/app/(auth)/talk/new/page.tsx
@@ -1,19 +1,28 @@
-export default function TalkNewPage() {
+import BlueSubmitButton from "@/components/buttons/BlueSubmitButton";
+import { submitNewTalk } from "./actions";
+import getSession from "@/libs/session";
+import { redirect } from "next/navigation";
+
+export default async function TalkNewPage() {
+  const user = (await getSession()).user;
+
+  if (!user) redirect("/");
+
   return (
     <main className="flex justify-center px-24 py-8">
       <div className="w-full max-w-[820px] rounded-xl bg-slate-200 p-[36px]">
-        <div className="flex w-full flex-col">
+        <form action={submitNewTalk} className="flex w-full flex-col">
           {/* 타이틀 */}
           <h1 className="ml-1 text-3xl font-bold">새 잡담</h1>
           <div className="my-2" />
           {/* 텍스트 부분 */}
-          <textarea className="min-h-96 w-full resize-none rounded-xl border-2 border-gray-300 bg-slate-50 p-4 outline-none" />
-          <div className="my-2" />
-
-          <button className="self-end rounded-xl bg-neutral-50 px-[26px] py-[8px] font-semibold">
-            등록
-          </button>
-        </div>
+          <textarea
+            className="min-h-96 w-full resize-none rounded-xl border-2 border-gray-300 bg-slate-50 p-4 outline-none"
+            name="content"
+          />
+          <div className="my-3" />
+          <BlueSubmitButton text="등록" />
+        </form>
       </div>
     </main>
   );
