3a83f40f35dcdf5072b0615b36703d896eb3e82f
ICE0208 <doky3307@naver.com>
2024-06-06 23:46:08 +0900

    feat: 버튼에 아이콘 추가

--- src/app/(auth)/page.tsx
+++ src/app/(auth)/page.tsx
@@ -1,3 +1,7 @@
+import ChatSVG from "@/icons/ChatSVG";
+import MapSVG from "@/icons/MapSVG";
+
+import ThumbUpSVG from "@/icons/ThumbUpSVG";
 import getSession from "@/libs/session";
 import Image from "next/image";
 import Link from "next/link";
@@ -18,16 +22,19 @@ export default async function Home() {
             height="1440"
           />
           <LinkButton
+            Icon={MapSVG}
             mainText="맛집"
             subText="먹고 싶은 맛집을 누구보다 빠르게"
             href="/food-map"
           />
           <LinkButton
+            Icon={ChatSVG}
             mainText="잡담"
             subText="음식을 가지고 다른 사람들과 즐겁게"
             href="/talk"
           />
           <LinkButton
+            Icon={ThumbUpSVG}
             mainText="리뷰"
             subText="내가 먹은 맛집을 다른 사람에게"
             href="/review"
@@ -39,10 +46,12 @@ export default async function Home() {
 }
 
 const LinkButton = ({
+  Icon,
   mainText,
   subText,
   href,
 }: {
+  Icon: JSX.Element;
   mainText: string;
   subText: string;
   href: string;
@@ -50,10 +59,13 @@ const LinkButton = ({
   return (
     <Link
       href={href}
-      className="z-0 flex size-80 cursor-pointer flex-col items-center justify-center gap-4 rounded-full bg-[#D9CBBC] p-12 ring-[12px] ring-[#FF914D] transition hover:scale-[102%]"
+      className="z-0 flex size-72 cursor-pointer flex-col items-center justify-center gap-4 rounded-full bg-[#D9CBBC] p-12 ring-[12px] ring-[#FF914D] transition hover:scale-[102%]"
     >
-      <span className="text-[50px] font-bold">{mainText}</span>
-      <span className="text-pretty text-center text-[22px] font-semibold">
+      <div className="flex w-full items-center justify-center gap-[6px]">
+        <div className="size-[50px]">{Icon}</div>
+        <div className="text-nowrap text-[50px] font-bold">{mainText}</div>
+      </div>
+      <span className="text-pretty text-center text-[22px] font-bold">
         {subText}
       </span>
     </Link>
