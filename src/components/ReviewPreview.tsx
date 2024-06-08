c488fea911f2f13c54de0585378da63bb3cb4b00
ICE0208 <doky3307@naver.com>
2024-06-08 22:00:26 +0900

    refactor: 별점 표기 컴포넌트화

--- src/components/ReviewPreview.tsx
+++ src/components/ReviewPreview.tsx
@@ -8,6 +8,7 @@ import { reviewPreviewActiveModalState } from "@/app/atoms";
 import { useCallback, useEffect } from "react";
 import { submitReviewLike } from "@/app/actions";
 import Link from "next/link";
+import RateStarDisplay from "./RateStarDisplay";
 
 interface ReviewPreviewProps {
   reviewId: number;
@@ -121,28 +122,5 @@ export default function ReviewPreview({
         </div>
       </div>
     </div>
-  );
-}
-
-interface RateStarDisplayProps {
-  rate: number;
-}
-
-function RateStarDisplay({ rate }: RateStarDisplayProps) {
-  // 평점(rate)은 5점 만점이므로, 만점을 100% 기준으로 맞춫기 위하여 x20을 해주었습니다.
-  const percentage = rate * 20;
-
-  return (
-    <span className="space-x-1 text-inherit">
-      <span>{(Math.floor(rate * 10) / 10).toFixed(1)}</span>
-      <span
-        style={{
-          backgroundImage: `linear-gradient(to right, #ffdd47 ${percentage}%, #cbcbcb ${percentage}%)`,
-        }}
-        className="bg-clip-text"
-      >
-        <span className="text-[#ffffff00]">★★★★★</span>
-      </span>
-    </span>
   );
 }
